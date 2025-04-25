26991e24d877c07995a9f1dc21f3f4ad8af6a8b3
Vishal Kumar <146015120+vishal-kr-dev@users.noreply.github.com>
2025-04-25 21:05:09 +0530

    Added: Leave routes

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -1,8 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { APIError } from "../utils/APIerror.js";
-import Employee from "../models/employee.model.js";
 import { APIresponse } from "../utils/APIresponse.js";
 import jwt from "jsonwebtoken";
+import Employee from "../models/employee.model.js";
+import LeaveModel from "../models/leave.model.js";
+import AttendanceModel from "../models/attendance.model.js";
 
 const generateAccessTokenAndRefreshToken = async (employeeId) => {
   try {
@@ -37,7 +39,7 @@ const loginEmployee = asyncHandler(async (req, res) => {
     throw new APIError(400, "Password is required");
   }
 
-  const employee = await Employee.findOne({email: email.toLowerCase()});
+  const employee = await Employee.findOne({ email: email.toLowerCase() });
 
   if (!employee) {
     throw new APIError(404, "Employee does not exist");
@@ -176,14 +178,17 @@ const getEmployeeProfile = asyncHandler(async (req, res) => {
       process.env.REFRESH_TOKEN_SECRET
     );
 
-    const employee = await Employee.findById({ _id: decodedToken._id })
-      .select("-password -refreshToken")
+    const employee = await Employee.findById({ _id: decodedToken._id }).select(
+      "-password -refreshToken"
+    );
 
     if (!employee) {
       throw new APIError(404, "Invalid credentials or employee not found");
     }
 
-    return res.json(new APIresponse(200, employee, "Employee details fetched successfully"))
+    return res.json(
+      new APIresponse(200, employee, "Employee details fetched successfully")
+    );
   } catch (error) {
     throw new APIError(
       400,
@@ -192,9 +197,144 @@ const getEmployeeProfile = asyncHandler(async (req, res) => {
   }
 });
 
+const applyLeave = asyncHandler(async (req, res) => {
+  const { _id } = req.employee;
+  const { leaveType, startDate, endDate, reason } = req.body;
+  if (!_id || !leaveType || !startDate || !endDate || !reason) {
+    throw new APIError(400, "All fields are required");
+  }
+
+  if (new Date(startDate) > new Date(endDate)) {
+    throw new APIError(400, "Start date cannot be after end date");
+  }
+  if (new Date(startDate) < new Date()) {
+    throw new APIError(400, "Start date cannot be in the past");
+  }
+
+  const employee = await Employee.findById(_id);
+  if (!employee) {
+    throw new APIError(404, "Employee not found");
+  }
+
+  try {
+    const leaveData = await LeaveModel.create({
+      adminId: employee.adminId,
+      employeeId: employee._id,
+      leaveType,
+      startDate,
+      endDate,
+      reason,
+    });
+
+    console.log(leaveData);
+
+    return res.json(
+      new APIresponse(200, leaveData, "Leave applied successfully")
+    );
+  } catch (error) {
+    throw new APIError(
+      500,
+      "Something went wrong while applying leave: " + error.message
+    );
+  }
+});
+
+const getLeaveHistory = asyncHandler(async (req, res) => {
+  const { _id } = req.body;
+
+  try {
+    const leaveHistory = await LeaveModel.aggregate([
+      {
+        $match: {
+          employeeId: new ObjectId(`${_id}`),
+        },
+      },
+      {
+        $lookup: {
+          from: "employees",
+          localField: "employeeId",
+          foreignField: "_id",
+          as: "employee",
+        },
+      },
+      {
+        $unwind: {
+          path: "$employee",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $project: {
+          employeeId: 1,
+          adminId: 1,
+          leaveType: 1,
+          startDate: 1,
+          endDate: 1,
+          reason: 1,
+          status: 1,
+          employeeName: "$employee.fullname",
+          department: "$employee.department",
+          designation: "$employee.designation",
+        },
+      },
+    ]);
+
+    if (!leaveHistory) {
+      throw new APIError(404, "No leave history found");
+    }
+  } catch (error) {
+    throw new APIError(
+      500,
+      "Something went wrong while fetching leave history: " + error.message
+    );
+  }
+
+  return res.json(
+    new APIresponse(200, leaveHistory, "Leave history fetched successfully")
+  );
+});
+
+const deleteLeave = asyncHandler(async (req, res) => {
+  const { leaveId } = req.body;
+  if (!leaveId) {
+    throw new APIError(400, "Leave ID is required");
+  }
+
+  const leave = await LeaveModel.findById(leaveId);
+  if (!leave) {
+    throw new APIError(404, "Leave not found");
+  }
+
+  await LeaveModel.findByIdAndDelete(leaveId);
+
+  return res.json(new APIresponse(200, {}, "Leave deleted successfully"));
+});
+
+const getAttendanceHistory = asyncHandler(async (req, res) => {
+  const employee = await AttendanceModel.findById(req.employee._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!employee) {
+    throw new APIError(404, "Employee not found");
+  }
+
+  return res.json(
+    new APIresponse(
+      200,
+      employee.attendanceHistory,
+      "Attendance history fetched successfully"
+    )
+  );
+});
+
 export {
   loginEmployee,
   logoutEmployee,
   refreshAccessToken,
   getEmployeeProfile,
+  applyLeave,
+  getLeaveHistory,
+  deleteLeave,
+  getAttendanceHistory,
 };
