6c09c3878334cccac2324d378d8dd285b913cd40
ysatyam129 <yadavsatyamsingh078@gmail.com>
2025-08-06 18:52:07 +0530

    cor 1

--- src/app.js
+++ src/app.js
@@ -9,10 +9,7 @@ const app = express()
 //     credentials:true,
 
 // }))
-app.use(cors({
-  origin:"https://attendance-management-system-fronte.vercel.app/",
-  credentials:true
-}));
+app.use(cors());
 app.use(express.json({
     limit:'16kb'
 }))
