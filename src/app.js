26b5491adbf4d9cdbeb1c0e2a46bdf6e3d4da782
ysatyam129 <yadavsatyamsingh078@gmail.com>
2025-08-06 19:14:22 +0530

    hardcoded url1

--- src/app.js
+++ src/app.js
@@ -4,11 +4,6 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials:true,
-
-}))
 app.use(cors());
 app.use(express.json({
     limit:'16kb'
